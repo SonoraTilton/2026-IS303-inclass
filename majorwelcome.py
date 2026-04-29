dd0a08311fbb3d6f1ad82e54792f4bb2d4c65f6f
Sonora Tilton <sonorakt@byu.edu>
2026-04-29 10:01:36 -0600

    created major welcome message

---
+++ majorwelcome.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+major = input("What is your major? ")
+
+print("Welcome to the class, " + name + "! Your major of " + major + " will be a great addition.")
+print(f"Welcome to the class, {name}! Your major of {major} will be a great addition.")
